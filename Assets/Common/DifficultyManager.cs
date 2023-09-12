10070dcab57601b28fc6713326702915bbb0e5b0
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2023-09-12 22:04:43 +0700

    EDIT: DifficultyManager V2.0 (Be able to control the amount of item spawning)

--- Assets/Common/DifficultyManager.cs
+++ Assets/Common/DifficultyManager.cs
@@ -1,13 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using PhEngine.Core;
 using UnityEngine;
+using UnityEngine.AI;
 
-namespace SuperGame
+namespace  SuperGame
 {
     public class DifficultyManager : Singleton<DifficultyManager>
     {
         public int DifficultyLevel => difficultyLevel;
+        public int HealthPotionSpawnDelay => healthPotionSpawnDelay;
+        public int ArmorSpawnDelay => armorSpawnDelay;
+        public int PoisonSpawnDelay => poisonSpawnDelay;
+
         [SerializeField] int difficultyLevel;
         [SerializeField] GameObject difficultyUI;
+        [SerializeField] int healthPotionSpawnDelay;
+        [SerializeField] int armorSpawnDelay;
+        [SerializeField] int poisonSpawnDelay;
         
         protected override void InitAfterAwake()
         {
@@ -17,6 +27,25 @@ namespace SuperGame
         public void SelectDifficultyLevel(int value)
         {
             difficultyLevel = value;
+
+            if (value == 1)
+            {
+                healthPotionSpawnDelay = 2;
+                armorSpawnDelay = 2;
+            }
+
+            if (value == 2)
+            {
+                healthPotionSpawnDelay = 1;
+                armorSpawnDelay = 1;
+                poisonSpawnDelay = 1;
+            }
+
+            if (value == 3)
+            {
+                poisonSpawnDelay = 2;
+            }
+
             difficultyUI.SetActive(false);
             GameManager.Instance.StartLevel();
         }
